4f43d316e3ddb2faacfb4619eac71c19986facd1
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-01 13:44:52 +0000

    All basic functionality

--- src/containers/Metronome.js
+++ src/containers/Metronome.js
@@ -8,8 +8,8 @@ import PlayPause from "../components/PlayPause";
 const Metronome = () => {
     const [bpm, setBPM] = useState(100);
     const [isPlaying, setIsPlaying] = useState(false);
-    // const [intervalId, setIntervalId] = useState(null);
     const intervalIdRef = useRef(null);
+    const audioRef = useRef(null);
 
     const handleBpmChange = (bpm) => {
         console.log("handleBpmChange", bpm)
@@ -22,20 +22,20 @@ const Metronome = () => {
     }
 
     const playBeat = () => {
-        console.log("Beat");
-        // audioSrc.current.play()
+        console.log("Beat", audioRef.current.ended, audioRef.current.currentTime);
+        audioRef.current.currentTime = 0;
+        audioRef.current.play()
     }
 
     useEffect(() => {
         if (isPlaying) {
             const milliSeconds = 60000 / bpm;
+            clearInterval(intervalIdRef.current);
             const newIntervalId = setInterval(() => playBeat(), milliSeconds);
-            // setIntervalId(newIntervalId);
             intervalIdRef.current = newIntervalId;
             console.log("called setInterval", newIntervalId, milliSeconds)
         } else {
             clearInterval(intervalIdRef.current);
-            // setIntervalId(null);
             intervalIdRef.current = null;
         }
     }, [bpm, isPlaying])
@@ -46,7 +46,7 @@ const Metronome = () => {
         <BPM bpm={bpm} />
         <PlayPause isPlaying={isPlaying} onClick={handleButtonClick} />
 
-        <audio src="/beat.wav" />
+        <audio ref={audioRef} src="/beat.wav" />
     </div>
 }
 
